8dd5cd07973f4db575cc2027b6a239705aba989a
timsinashok <ashoktimsina147181@gmail.com>
2025-06-17 15:11:36 -0700

    removed deprecated appDir from config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true, // Enables App Router support
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
